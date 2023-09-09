c3e8e6bd4882d32e4f5db2734987281fae474272
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2023-09-09 06:44:10 -0600

    fist changes

--- .history/src/App_20230909063408.vue
+++ .history/src/App_20230909063408.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/qslbureau.png">
-  <HelloWorld msg="QSL buro FMRE.mx"/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
--- .history/src/App_20230909064052.vue
+++ .history/src/App_20230909064052.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/qslbureau.png">
-  <HelloWorld msg="QSL buro FMRE.mx"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
